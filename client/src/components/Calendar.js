11711280ce3f59269e40fc0f33abc223818a7728
Sandesh <chapagainsandesh88@gmail.com>
2021-02-23 18:34:28 -0500

    show calendars based on active days

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { Row, Col } from 'reactstrap';
 import Day from './Day';
 
-const Calendar = (props) => {
-    const days = new Array(35).fill(0).map((d,i) => i + 1);
-    const weeks = [0,1,2,3,4];
-    const nameOfDays = ["Mon","Tues","Wed","Thu","Fri","Sat","Sun"];
+const Calendar = ({begin,activeDays,end,toggleDone}) => {
+
+  const days = activeDays.slice(begin,end+1);
+  const weeks = [0,1,2,3,4];
+  const nameOfDays = ["Mon","Tues","Wed","Thu","Fri","Sat","Sun"];
   return (
     <Col className="Calendar m-5" xs={12} sm={12} md={12} lg={6}>
         <Row>
-            {nameOfDays.map(name => <Col>{name}</Col>)}
+            {nameOfDays.map(name => <Col key={name}>{name}</Col>)}
         </Row>
       {
           
-          weeks.map(week =><Row>
-          {days.slice(week*7,week*7 + 7).map(day => <Day day={day}/>)}</Row>)
+          weeks.map(week =><Row key={week}>
+          {days.slice(week*7,week*7 + 7).map(day => <Day toggleDone={toggleDone} key={day.day.toString()} day={day}/>)}</Row>)
       }
     </Col>
   );
--- client/src/components/CalendarGrid.js
+++ client/src/components/CalendarGrid.js
@@ -2,12 +2,37 @@
 import React from 'react';
 import { Container, Row } from 'reactstrap';
 import Calendar from './Calendar';
-function CalendarGrid() {
+
+function CalendarGrid({activeDays,toggleDone}) {
+
+  const firstDay = activeDays[0].day;
+  const lastDay = activeDays[activeDays.length - 1].day;
+
+  const needExtraMonth = firstDay.startOf("month") !== lastDay.startOf("month");
+
+  let nextMonthStart = 0;
+  if (needExtraMonth) {
+    for (let i = 1; i < activeDays.length; i++) {
+      if (activeDays[i].day.month !== activeDays[i-1].day.month) {nextMonthStart = i;break}
+    }
+  }
+
   return (
       <Container>
           <Row className="justify-content-center">
-            <Calendar/> 
-            <Calendar/>
+            <Calendar 
+            begin={0} 
+            activeDays = {[...activeDays]}
+            end={needExtraMonth? nextMonthStart - 1 : activeDays.length - 1}
+            toggleDone={toggleDone}
+            /> 
+            {needExtraMonth && 
+            <Calendar 
+            begin={nextMonthStart}
+            activeDays = {activeDays}
+            end = {activeDays.length - 1}
+            toggleDone = {toggleDone}
+            />}
           </Row>
       </Container>
      
