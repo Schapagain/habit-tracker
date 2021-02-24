2e30d5c67ed204410dc3af3b7a7cb67bca6f0021
Sandesh <chapagainsandesh88@gmail.com>
2021-02-23 22:32:07 -0500

    refactor to HabitBlock

--- client/src/components/CalendarGrid.js
+++ client/src/components/CalendarGrid.js
@@ -1,41 +1,15 @@
 
 import React from 'react';
-import { Container, Row } from 'reactstrap';
+import { Container } from 'reactstrap';
 import Calendar from './Calendar';
 
-function CalendarGrid({activeDays,toggleDone}) {
-
-  const firstDay = activeDays[0].day;
-  const lastDay = activeDays[activeDays.length - 1].day;
-
-  const needExtraMonth = firstDay.startOf("month") !== lastDay.startOf("month");
-
-  let nextMonthStart = 0;
-  if (needExtraMonth) {
-    for (let i = 1; i < activeDays.length; i++) {
-      if (activeDays[i].day.month !== activeDays[i-1].day.month) {nextMonthStart = i;break}
-    }
-  }
+function CalendarGrid({block,toggleDone}) {
+  const calendars = block.calendarDays;
 
   return (
       <Container>
-          <Row className="justify-content-center">
-            <Calendar 
-            begin={0} 
-            activeDays = {[...activeDays]}
-            end={needExtraMonth? nextMonthStart - 1 : activeDays.length - 1}
-            toggleDone={toggleDone}
-            /> 
-            {needExtraMonth && 
-            <Calendar 
-            begin={nextMonthStart}
-            activeDays = {activeDays}
-            end = {activeDays.length - 1}
-            toggleDone = {toggleDone}
-            />}
-          </Row>
+          {calendars && calendars.map((calendar,i) => <Calendar key={i} block={block} toggleDone={toggleDone} days={calendar}/>)}
       </Container>
-     
   );
 }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,50 +1,23 @@
-import React,{useState} from 'react';
+import React,{useEffect, useRef, useState} from 'react';
 import CalendarGrid from './CalendarGrid';
-import { DateTime, Interval } from 'luxon';
+import HabitBlock from '../classes/HabitBlock';
 
-function* days(interval) {
-  let cursor = interval.start.startOf("day");
-  while (cursor < interval.end) {
-    yield cursor;
-    cursor = cursor.plus({days: 1});
-  }
-}
-
-class Day {
-  constructor(day,done,active = true) {
-    this.day = day;
-    this.done = done;
-    this.active = active;
-  }
-}
-
-const currentWindowStart = DateTime.now().startOf('day');
-const currentWindowEnd = currentWindowStart.plus({days:29});
-
-let intervalStart =  currentWindowStart.startOf("month");
-let intervalEnd = currentWindowEnd.endOf("month");
-
-const firstWeekDay = intervalStart.weekday - 1;
-if (firstWeekDay > 0) {
-  intervalStart = intervalStart.minus({days:firstWeekDay});
-}
-
-const lastWeekDay = intervalEnd.weekday - 1;
-if (lastWeekDay < 6) {
-  intervalEnd = intervalEnd.plus({days:6-lastWeekDay})
-}
-const interval = Interval.fromDateTimes(intervalStart,intervalEnd);
-
-let active = [];
-let dayIsActive;
-for (let day of days(interval)){
-  dayIsActive = ( day >= currentWindowStart) && (day <= currentWindowEnd);
-  active.push(new Day(day,false,dayIsActive));
-}
+const newHabitBlock = new HabitBlock();
 const Home = (props) => {
-  const [activeDays,setActiveDays] = useState(active);
+  const [block,setBlock] = useState(newHabitBlock);
+
+  let numDone = useRef(0);
+
+  useEffect(() => {
+    numDone.current = block.doneDays;
+    console.log('in effect:',numDone.current)
+  });
+
   return (
-    <CalendarGrid toggleDone={setActiveDays} activeDays ={[...activeDays]}/>
+    <>
+    <h1>{numDone.current}</h1>
+    <CalendarGrid toggleDone={setBlock} block={block}/>
+    </>
   );
 }
 
