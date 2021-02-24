64ac28891658a5039a1c20c634efd1f55f3f45c2
Sandesh <chapagainsandesh88@gmail.com>
2021-02-24 12:58:35 -0500

    adjust layout in grid

--- client/src/components/CalendarGrid.js
+++ client/src/components/CalendarGrid.js
@@ -1,15 +1,15 @@
 
 import React from 'react';
-import { Container } from 'reactstrap';
+import { Col } from 'reactstrap';
 import Calendar from './Calendar';
 
 function CalendarGrid({block,toggleDone}) {
   const calendars = block.calendarDays;
 
   return (
-      <Container>
+      <Col xs="12" sm="12" md="8" lg="6" xl="5">
           {calendars && calendars.map((calendar,i) => <Calendar key={i} block={block} toggleDone={toggleDone} days={calendar}/>)}
-      </Container>
+      </Col>
   );
 }
 
