606a9083ae4db0caf20a50f29653ea8d0dda00a6
Sandesh <chapagainsandesh88@gmail.com>
2021-02-24 12:56:51 -0500

    minor edits

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,14 @@
 import { Container } from 'reactstrap';
 import './App.css';
-import Home from './components/Home.js';
 import 'bootstrap/dist/css/bootstrap.css';
+import AppHome from './components/AppHome.js';
 
 function App() {
   return (
-    <Container className="App">
-      <h1 className="heading"> Habit Tracker</h1>
-       <Home/>
+    <Container fluid className="App">
+      <h1 className="heading p-5"> Habit Tracker</h1>
+       <AppHome />
     </Container>
-      
-
   );
 }
 
