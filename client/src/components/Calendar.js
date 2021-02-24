839ebaa0c5a1fc6e91b234ea847f0b20f058de66
Sandesh <chapagainsandesh88@gmail.com>
2021-02-24 12:59:12 -0500

    pass habit block down children and toggle days accordingly

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Container } from 'reactstrap';
 import Day from './Day';
 
-const Calendar = ({begin,activeDays,end,toggleDone}) => {
+const Calendar = ({days,block,toggleDone}) => {
 
-  const days = activeDays.slice(begin,end+1);
   const weeks = [0,1,2,3,4];
   const nameOfDays = ["Mon","Tues","Wed","Thu","Fri","Sat","Sun"];
+
   return (
-    <Col className="Calendar m-5" xs={12} sm={12} md={12} lg={6}>
-        <Row>
-            {nameOfDays.map(name => <Col key={name}>{name}</Col>)}
-        </Row>
+    <Container className="calendar mb-5">
+      <Row className="center">
+        <h1>{days[7].date.monthShort}</h1>
+      </Row>
+      <Row className="mb-1">
+          {nameOfDays.map(name => <Col key={name}>{name}</Col>)}
+      </Row>
       {
-          
-          weeks.map(week =><Row key={week}>
-          {days.slice(week*7,week*7 + 7).map(day => <Day toggleDone={toggleDone} key={day.day.toString()} day={day}/>)}</Row>)
+          weeks.map(week =>
+          <Row key={week}>
+            {days.slice(week*7,week*7 + 7).map(day => 
+            <Day block={block} toggleDone={toggleDone} key={day.date.toString()} day={day}/>)}
+          </Row>)
       }
-    </Col>
+    </Container>
   );
 }
 
--- client/src/components/Day.js
+++ client/src/components/Day.js
@@ -1,15 +1,37 @@
-import React,{useState} from 'react';
+import { DateTime } from 'luxon';
+import React from 'react';
 import { Col } from 'reactstrap';
 
-const Day = ({day,toggleDone}) => {
-  const dayNum = day.day.day;
+const Day = ({day,block,toggleDone}) => {
+  const dayNum = day.date.day;
   const done = day.done;
   const active = day.active;
-  const [marked,toggleMarked] = useState(done);
+  const inFuture = DateTime.now() < day.date;
+
   return (
     <Col 
-    onClick={() => {if(active) toggleMarked(!marked)}} 
-    className={`day ${active? (marked ? "marked":"unmarked" ) : "inactive"}`}>{dayNum}</Col>
+    onClick={() => {
+
+      if (active && !inFuture) {
+        block.toggleDay(day.date);
+        let clone = Object.assign(Object.create(Object.getPrototypeOf(block)), block)
+        toggleDone(clone);
+      }
+
+    }} 
+    className={`day ${
+      active
+      ? (
+        inFuture 
+        ? "active future" 
+        : done 
+          ? "active marked"
+          :"active unmarked" ) 
+      : "inactive"}`}
+      >
+      
+      {dayNum}
+    </Col>
   );
 };
 
