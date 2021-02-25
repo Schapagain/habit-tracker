4810b3895933851a5b5cdf1c62938312c0574fab
Sandesh <chapagainsandesh88@gmail.com>
2021-02-24 21:32:39 -0500

    add react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Route, Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ <BrowserRouter>
+  <Switch>
+   <Route path="/" component={App} />
+  </Switch>
+ </BrowserRouter>,
+ document.getElementById("root")
 );
