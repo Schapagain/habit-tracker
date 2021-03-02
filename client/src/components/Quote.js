b07f005bce5740a4f3eb9304793ee3e75e1250c5
Sandesh <chapagainsandesh88@gmail.com>
2021-03-01 20:41:42 -0500

    new component to display daily quotes

---
+++ client/src/components/Quote.js
@@ -0,0 +1,15 @@
+
+
+const Quote = () => {
+
+    return (
+        <div className="mb-2">
+            <p>
+                "You do not rise to the level of your goals. You fall to the level of your systems."
+            </p>
+            <p> ― James Clear, Atomic Habits</p>
+        </div>
+    )
+}
+
+export default Quote;
