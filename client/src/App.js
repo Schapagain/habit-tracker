ae260e01c056a175cf06ecf548ce7069110108c8
Sandesh <chapagainsandesh88@gmail.com>
2021-03-01 20:45:33 -0500

    replace bootstrap layouts with tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
-import { Container } from 'reactstrap';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
 import AppHome from './components/AppHome.js';
+import {faCalendarPlus} from '@fortawesome/free-solid-svg-icons';
+import {library} from '@fortawesome/fontawesome-svg-core';
+library.add(faCalendarPlus);
+
 
 function App() {
   return (
-    <Container fluid className="App">
-      <h1 className="heading p-5"> Habit Tracker</h1>
-       <AppHome />
-    </Container>
+    <div className="min-w-min-lg h-screen mx-auto overflow-x-hidden max-w-screen-xl flex flex-col text-center justify-between bg-indigo-400 min-h-screen">
+      <AppHome />
+      <h6>This is a crazy footer</h6>
+    </div>
   );
 }
 
