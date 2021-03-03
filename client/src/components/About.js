1a747a51e9efc39f1d5da5a539181426783ddd8e
Sandesh <chapagainsandesh88@gmail.com>
2021-03-03 13:11:48 -0500

    update stylings

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,10 +1,26 @@
+import BackButton from "./BackButton";
+import Panel from "./Panel";
 
 const About = () => {
     return (
-        <div className="h-full m-auto">
-            <p>This is a simple application to help you track your habits on a daily basis.<br/>
-                 Taste the cumulative effects of being aware of your day-to-day actions.</p>
+        <div className="flex h-full w-full justify-center m-auto">
+            <BackButton
+            className="my-auto mr-3 hover:-translate-x-1" 
+            />
+            <Panel 
+            className = "w-full"
+            content={<AboutText/>} 
+            />
         </div>
+    )
+}
+
+const AboutText = () => {
+    return(
+        <p className="text-white p-5">
+                This is a simple application to help you track your habits on a daily basis.<br/>
+                Taste the cumulative effects of being aware of your day-to-day actions.
+        </p>
     )
 }
 
--- client/src/components/Features.js
+++ client/src/components/Features.js
@@ -1,12 +1,34 @@
+import BackButton from "./BackButton";
+import Panel from "./Panel";
 
 const Features = () => {
     return (
-        <div className="h-full text-left m-auto">
-            <ul>
-                <li> - Track multiple habits by crossing out calendars in respective blocks </li>
-                <li> - Add notes and reflections to each day</li>
-            </ul>
+        <div className="flex h-full w-full justify-center m-auto">
+            <BackButton
+            className="my-auto mr-3 hover:-translate-x-1" 
+            />
+            <Panel 
+            className = "w-full"
+            content={<FeaturesText/>} 
+            />
         </div>
+    )
+}
+
+const FeaturesText = () => {
+    return(
+        <div className="text-white p-5">
+            <p className="text-xl p-3">With this application you can:</p>
+            <ol className="list-disc list-inside">
+                <li>
+                    Mark calendars for multiple habits simultaneously
+                </li>
+                <li>
+                    Add daily reflections to each habit
+                </li>
+            </ol> 
+        </div>
+        
     )
 }
 
