bae4f0f768a65e67720c77eaf90d6dc7a61f4c19
Sandesh <chapagainsandesh88@gmail.com>
2021-03-03 16:55:01 -0500

    alter layout

--- client/src/components/HabitHome.js
+++ client/src/components/HabitHome.js
@@ -2,16 +2,27 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import HabitBlock from '../utils/HabitBlock';
 import CalendarGrid from './CalendarGrid';
-
+import NavBar from './NavBar';
+import HabitInsights from './HabitInsights';
+import BackButton from './BackButton';
 
 const HabitHome = ({location}) => {
     const habit = location.state && location.state.habit;
-    const mainClass = "flex flex-col w-full"
   return habit && habit instanceof HabitBlock ? (
-    <div className={mainClass}>
-        <div className="relative mb-5 m-auto w-full sm:w-2/3 lg:w-1/2 h-full">
+    <div className="flex min-h-screen flex-col w-full">
+          <NavBar />
+          <div className="flex m-auto">
+            <BackButton className="my-auto hover:-translate-x-1"/>
+           <div className="m-auto flex flex-col justify-center">
+            <div className="p-4">
+              <p className="text-3xl">{habit.name}</p>
+              <p>{habit.description}</p>
+            </div>
             <CalendarGrid block={habit} />
-        </div> 
+            <HabitInsights habit={habit}/>  
+          </div> 
+          </div>
+          
       </div>
   ) : (
       <Redirect to="/home" />
