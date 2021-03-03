9eb649086d4cb76d266176e64ad6c95328b06798
Sandesh <chapagainsandesh88@gmail.com>
2021-03-02 22:35:49 -0500

    add text to landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,49 @@
+import NavBar from './NavBar';
+import { useHistory } from 'react-router-dom';
+import { FaCalendarPlus } from 'react-icons/fa';
+import { RiRepeatOneLine } from 'react-icons/ri';
 
-import { Link } from 'react-router-dom';
+const IntroText = () => {
+    return(
+        <div>
+            <p>Keepings habits simple.</p>
+            <p>Track your habits,</p>
+            <p>block by block.</p>
+        </div>
+    )
+}
+
+const Button = ({text,icon}) => {
+    const history = useHistory();
+    const goToSignup = () => history.push("/signup");
+    return (
+        <a 
+        className = "flex bg-gray-300 text-white bg-gray-600 hover: rounded-xl p-2 mx-auto m-2"
+        href = "!#" 
+        onClick={(e)=>{e.preventDefault();goToSignup()}} 
+        >
+            {icon && <p className="mx-2 my-auto">{icon}</p>}<p>{text}</p>
+        </a>
+    )
+}
 
 const Landing = () => {
     return (
-        <div>
-            <h1 className="m-2">This is the Landing.</h1>
-            <Link className="bg-gray-200 p-2 mt-4 rounded-lg hover:text-white hover:bg-gray-700"  to="/home" >Go to Home</Link>
+        <div className="min-h-screen justify-between flex flex-col bg-indigo-500">
+            <NavBar />
+            <div className="flex my-auto justify-center">
+                <div className="flex m-3 flex-col">
+                    <IntroText />
+                    <Button text="Get started" />    
+                </div>    
+                <div className="m-2 flex flex-col">
+                    <Button text="Add a habit" icon={<FaCalendarPlus/>} />
+                    This will have pictures later
+                    <Button text="Keep at it!" icon={<RiRepeatOneLine />} />
+                </div>
+            </div>
+            
+            
         </div>
     )
 }
