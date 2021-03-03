33aec0577ee9f718b9460aec108fc59ff1b78bea
Sandesh <chapagainsandesh88@gmail.com>
2021-03-02 22:34:59 -0500

    configure craco

--- client/tailwind.js
+++ client/tailwind.js
@@ -15,7 +15,7 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-
+      'spring-rain': '#aec3b3',
       black: colors.black,
       white: colors.white,
       gray: colors.coolGray,
