05c163b9c8c4ef4d8650bf71c0902f06b69727fa
Sandesh <chapagainsandesh88@gmail.com>
2021-03-02 22:36:01 -0500

    add temp about and features page

---
+++ client/src/components/About.js
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return (
+        <div className="h-full m-auto">
+            <p>This is a simple application to help you track your habits on a daily basis.<br/>
+                 Taste the cumulative effects of being aware of your day-to-day actions.</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ client/src/components/Features.js
@@ -0,0 +1,13 @@
+
+const Features = () => {
+    return (
+        <div className="h-full text-left m-auto">
+            <ul>
+                <li> - Track multiple habits by crossing out calendars in respective blocks </li>
+                <li> - Add notes and reflections to each day</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Features;
