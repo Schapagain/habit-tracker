8c956a23d9c4a03b7ffc5bb6e4515185b7009e56
Sandesh <chapagainsandesh88@gmail.com>
2021-03-03 10:16:56 -0500

    add calendar icons and button animations

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,13 +2,17 @@ import NavBar from './NavBar';
 import { useHistory } from 'react-router-dom';
 import { FaCalendarPlus } from 'react-icons/fa';
 import { RiRepeatOneLine } from 'react-icons/ri';
+import CalendarIcon from './CalendarIcon';
+import { BsArrowUpDown, BsArrowDown } from 'react-icons/bs';
 
 const IntroText = () => {
     return(
         <div>
-            <p>Keepings habits simple.</p>
-            <p>Track your habits,</p>
-            <p>block by block.</p>
+            <p className="text-4xl mb-4">Keepings habits simple.</p>
+            <div className="text-2xl">
+                <p>Track your habits,</p>
+                <p>block by block.</p>    
+            </div>
         </div>
     )
 }
@@ -18,7 +22,10 @@ const Button = ({text,icon}) => {
     const goToSignup = () => history.push("/signup");
     return (
         <a 
-        className = "flex bg-gray-300 text-white bg-gray-600 hover: rounded-xl p-2 mx-auto m-2"
+        className = {`flex bg-gray-300 text-white bg-gray-600
+        hover:bg-calypso hover:animate-bounce rounded-xl p-3 mx-auto m-3
+        transition duration-500 transform ease-in-out hover:scale-110
+        box-shadow hover:shadow-2xl`}
         href = "!#" 
         onClick={(e)=>{e.preventDefault();goToSignup()}} 
         >
@@ -28,22 +35,23 @@ const Button = ({text,icon}) => {
 }
 
 const Landing = () => {
+    const arrowStyles = "m-auto text-2xl text-calypso";
     return (
-        <div className="min-h-screen justify-between flex flex-col bg-indigo-500">
+        <div className="min-h-screen justify-between flex flex-col">
             <NavBar />
-            <div className="flex my-auto justify-center">
-                <div className="flex m-3 flex-col">
+            <div className="flex h-2/3 my-auto justify-center">
+                <div className="flex my-auto m-3 w-1/2 flex-col">
                     <IntroText />
                     <Button text="Get started" />    
-                </div>    
-                <div className="m-2 flex flex-col">
+                </div>
+                <div className="m-2 flex w-1/2 flex-col">
                     <Button text="Add a habit" icon={<FaCalendarPlus/>} />
-                    This will have pictures later
+                    <BsArrowDown className={arrowStyles} />
+                    <CalendarIcon />
+                    <BsArrowUpDown className={arrowStyles} />
                     <Button text="Keep at it!" icon={<RiRepeatOneLine />} />
                 </div>
             </div>
-            
-            
         </div>
     )
 }
