94af1f01274e7bdc844a50043a3df190f003d137
Sandesh <chapagainsandesh88@gmail.com>
2021-03-03 13:12:22 -0500

    add wrapper component to display in panels

---
+++ client/src/components/Panel.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import classNames from 'classnames';
+
+export const Panel = ({className,content}) => {
+
+    const mainClass = classNames(className,
+        "flex flex-col justify-center h-1/2 mx-4 my-auto rounded-xl p-4 bg-calypso w-full sm:w-1/2 lg:w-1/3"
+        )
+    return (
+        <div className = {mainClass}>
+            {content}
+        </div>
+    )
+}
+
+export default Panel;
