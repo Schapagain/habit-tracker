33ea91eb682a75faf542f775cf1b4208cf4008e7
Sandesh <chapagainsandesh88@gmail.com>
2021-03-03 16:55:17 -0500

    increase day cell size

--- client/src/components/Day.js
+++ client/src/components/Day.js
@@ -15,11 +15,11 @@ const Day = ({day,block,toggleDone,isDayName}) => {
     dayName = day;
   }
 
-  const mainClass = classNames('w-7 h-7 flex justify-center flex-col m-auto',{
+  const mainClass = classNames('w-10 h-10 flex justify-center flex-col m-auto',{
     'cursor-pointer' : !isDayName && active,
     'cursor-not-allowed' : !isDayName && inFuture,
     'hover:bg-gray-700' : !isDayName && !inFuture,
-    'text-gray-600' : !isDayName && !active,
+    'text-gray-400' : !isDayName && !active,
   })
 
   return (
@@ -36,7 +36,7 @@ const Day = ({day,block,toggleDone,isDayName}) => {
     className={mainClass}
       >
       {dayNum || dayName}
-      {dayNum && done && active && <div className="w-7 h-7 m-auto absolute text-2xl">X</div>}
+      {dayNum && done && active && <div className="w-10 h-10 m-auto absolute text-2xl">X</div>}
     </div>
   );
 };
