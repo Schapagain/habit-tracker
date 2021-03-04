fe700fda6c3f4aa49cbdf10203128005f7896b20
Sandesh <chapagainsandesh88@gmail.com>
2021-03-03 21:30:17 -0500

    change layouts

--- client/src/components/CalendarGrid.js
+++ client/src/components/CalendarGrid.js
@@ -5,7 +5,7 @@ import Calendar from './Calendar';
 function CalendarGrid({block}) {
   const calendars = block && block.calendarDays;
     return (
-        <div className="p-2 mx-auto flex text-white bg-calypso rounded-2xl">
+        <div className="p-2 mx-auto md:flex-row flex flex-col text-white bg-calypso rounded-2xl">
           {calendars && calendars.map((calendar,i) => <Calendar key={i} block={block} days={calendar}/>)}
         </div>
     );
