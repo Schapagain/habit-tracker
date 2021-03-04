554a14bb0852dd00c4c198e16a75c225a623e74c
Sandesh <chapagainsandesh88@gmail.com>
2021-03-03 21:29:44 -0500

    refactor code and insert overlapping days between calendars

--- client/src/utils/HabitBlock.js
+++ client/src/utils/HabitBlock.js
@@ -84,37 +84,80 @@ class HabitBlock {
     return activeDays;
   }
 
+  _getOverlappedDays() {
 
+    let usingTwoMonths = this._numActiveFirstMonth < HabitBlock.#NUM_DAYS_IN_BLOCK;
+    if (!usingTwoMonths) return [];
 
-  _makeCalendarDays() {
-    let startMonthBegin =  this._startDate.startOf("month");
+    // These will create overlaps in two calendars
+    let startMonthEnd = this._startDate.endOf("month");
+    let endMonthStart = this._endDate.startOf("month");
+
+    // Find the number of missing days from each calendar
+    let numMissingFirstMonth = 7 - startMonthEnd.weekday;
+    let numMissingSecondMonth = startMonthEnd.weekday % 7;
+
+    // Find all the overlapped days and insert them as inactive days in the middle of two calendars
+    let afterFirstMonthEnd = Interval.after(endMonthStart,{days:numMissingFirstMonth});
+    let beforeSecondMonthStart = Interval.before(endMonthStart,{days:numMissingSecondMonth});
+
+    let overlaps = [];
+    overlaps.push(...this._getDaysFromInterval(afterFirstMonthEnd));
+    overlaps.push(...this._getDaysFromInterval(beforeSecondMonthStart));
+    return {overlaps,offset:numMissingFirstMonth};
+  }
+
+  _getDaysFromInterval(interval,isActive=false) {
+    let days = [];
+    for (let date of dateGenerator(interval)) {
+      const day = new Day(date,false,isActive);
+      days.push(day);
+    }
+    return days;
+  }
+
+  _getSurroundingDays() {
+    let startMonthStart =  this._startDate.startOf("month");
     let endMonthEnd = this._endDate.endOf("month");
 
     // adjust for months not starting on monday, or not ending on sunday
     endMonthEnd = endMonthEnd.plus({days:7-endMonthEnd.weekday});
-    startMonthBegin = startMonthBegin.minus({days: startMonthBegin.weekday - 1});
+    startMonthStart = startMonthStart.minus({days: startMonthStart.weekday - 1}); 
 
-    let beforeStartDate = Interval.fromDateTimes(startMonthBegin,this._startDate);
+    // Find all days before the startDate and after the endDate 
+    // and insert them around the activedays as dummies
+    let beforeStartDate = Interval.fromDateTimes(startMonthStart,this._startDate);
     let afterEndDate = Interval.fromDateTimes(this._endDate,endMonthEnd);
+    let before = this._getDaysFromInterval(beforeStartDate);
+    let after = this._getDaysFromInterval(afterEndDate);
+    return {before,after};
+  }
 
-    let before = [], after =[];
-    for (let date of dateGenerator(beforeStartDate)) {
-      const day = new Day(date,false,false);
-      before.push(day);
-    }
+  _makeCalendarDays() {
 
-    for (let date of dateGenerator(afterEndDate)) {
-      const day = new Day(date,false,false);
-      after.push(day);
+    let firstMonthEndIndex = this._numActiveFirstMonth;
+    let usingTwoMonths = firstMonthEndIndex < HabitBlock.#NUM_DAYS_IN_BLOCK;
+    let activeDays = [...this._activeDays];
+
+    // Insert any days that overlap between two months
+    // Offset firstmonth end by the number of dummy days inserted
+    if (usingTwoMonths) {
+      const {overlaps,offset} = this._getOverlappedDays();
+      activeDays.splice(firstMonthEndIndex,0,...overlaps);  
+      firstMonthEndIndex += offset;
     }
+    
+    // Get the days before the startDate and after the endDate 
+    // that should display on the calendar as inactive days
+    const {before,after} = this._getSurroundingDays();
 
     // return one or two calenders as needed for the block
     let calendars = [];
-    if (this._numActiveFirstMonth === HabitBlock.#NUM_DAYS_IN_BLOCK) {
-      calendars.push([...before,...this._activeDays,...after])
+    if (!usingTwoMonths) {
+      calendars.push([...before,...activeDays,...after])
     } else {
-      calendars.push([...before,...this._activeDays.slice(0,this._numActiveFirstMonth)])
-      calendars.push([...this._activeDays.slice(this._numActiveFirstMonth),...after]);
+      calendars.push([...before,...activeDays.slice(0,firstMonthEndIndex)])
+      calendars.push([...activeDays.slice(firstMonthEndIndex),...after]);
     }
     return calendars;
   }
