23e774bfa2667f162c93348e9521081ff6d1039f
Sandesh <chapagainsandesh88@gmail.com>
2021-03-07 16:35:49 -0500

    add user CRUD routes

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,137 @@
 const express = require('express');
 const router = express.Router();
-const { urlGoogle, getGoogleUser } = require('../controllers/handleGoogleLogin');
+const auth = require("../middlewares/auth");
 
-router.get("/",(req,res) => {
+const {
+    getUsers,
+    signupUser,
+    updateUser,
+    deleteUser,
+  } = require("../controllers/users");
 
-    const {token} = req.headers;
-    if (!token) {
-        return res.status(400).json({
-            error : "Invalid token"
-        })
+/**
+ * Route to fetch all users
+ * @name api/users
+ * @method GET
+ * @access ADMIN
+ * @inner
+ * @param {string} path
+ * @param {callback} middleware - Authenticate
+ * @param   {callback} middleware - Handle HTTP response
+ */
+router.get("/", async (req, res) => {
+    try {
+    //   const query = req.auth.role === ADMIN ? {} : { id: req.auth.id };
+      const result = await getUsers({ query : {} });
+      res.status(200).json(result);
+    } catch (err) {
+        console.log(err)
+      res.status(err.httpCode || 500).json({
+        error: {
+          msg: err.message,
+        },
+      });
     }
-    res.status(200).json({
-        user : getGoogleUser()
+  });
+
+/**
+ * Route to sigup a user
+ * @name    api/users/signup
+ * @method  POST
+ * @access  Public
+ * @inner
+ * @param   {string} path
+ * @param   {callback} middleware - Form Parser
+ * @param   {callback} middleware - Handle HTTP response
+ */
+router.post("/signup", async (req, res) => {
+  try {
+    const result = await signupUser(req.body);
+    res.status(201).json(result);
+  } catch (err) {
+    return res.status(err.httpCode || 500).json({
+      error: {
+        field: err.field,
+        msg: err.message,
+      },
     });
-})
+  }
+});
+
+/**
+ * Route to fetch user details
+ * @name api/users/:id
+ * @method GET
+ * @access Public
+ * @inner
+ * @param {string} path
+ * @param {callback} middleware - Authenticate
+ * @param   {callback} middleware - Handle HTTP response
+ */
+router.get("/:id", async (req, res) => {
+  try {
+    const attributes = [
+      "id",
+      "name",
+      "email",
+      "username"
+    ];
+    let result = await getUsers({
+      query: { id: req.params.id },
+      attributes,
+    });
+    res.status(200).json(result);
+  } catch (err) {
+    return res.status(err.httpCode || 500).json({
+      error: { msg: err.message },
+    });
+  }
+});
+
+/**
+ * Route to update user info
+ * @name    api/users/:id
+ * @method  PATCH
+ * @access  Private
+ * @inner
+ * @param   {string} path
+ * @param   {callback} middleware - Authenticate
+ * @param   {callback} middleware - Handle HTTP response
+ */
+router.patch("/:id", async (req, res) => {
+  try {
+    let result = await updateUser({ ...req.body, id: req.params.id });
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(err.httpCode || 500).json({
+      error: { msg: err.message },
+    });
+  }
+});
+
+/**
+ * Route to delete a user
+ * @name    api/users
+ * @method  DELETE
+ * @access  ADMIN
+ * @inner
+ * @param   {string} path
+ * @param   {callback} middleware - Form Parser
+ * @param   {callback} middleware - Handle HTTP response
+ */
+router.delete(
+  "/:id",
+  // auth([ADMIN]),
+  async (req, res) => {
+    try {
+      const result = await deleteUser(req.params.id);
+      res.status(200).json(result);
+    } catch (err) {
+      return res.status(err.httpCode || 500).json({
+        error: { msg: err.message },
+      });
+    }
+  }
+);
 
 module.exports = router;
