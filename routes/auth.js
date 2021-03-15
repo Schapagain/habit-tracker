a9a090449f05e4c41222feedc6e8723ec9bf234a
Sandesh <chapagainsandesh88@gmail.com>
2021-03-14 21:11:48 -0400

    add functions to reset password and request an OTP

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { urlGoogle } = require("../controllers/handleGoogleLogin");
-const { authenticate, authenticateAdmin } = require("../controllers/auth");
+const {
+  authenticate,
+  authenticateAdmin,
+  sendOTP,
+  resetPassword,
+} = require("../controllers/auth");
 router.get("/google", (req, res) => {
   res.status(200).json({
     url: urlGoogle(),
@@ -10,12 +15,11 @@ router.get("/google", (req, res) => {
 
 /**
  * Route to authenticate admin credentials
- * @name api/auth/admin
+ * @name auth/admin
  * @method POST
  * @access Public
  * @inner
  * @param {string} path
- * @param {callback} middleware - Form Parser
  * @param   {callback} middleware - Handle HTTP response
  */
 router.post("/admin", async (req, res) => {
@@ -28,13 +32,50 @@ router.post("/admin", async (req, res) => {
 });
 
 /**
+ * Route to request an OTP incase the user forgot their password
+ * @name api/auth/forget_password
+ * @method POST
+ * @access Public
+ * @inner
+ * @param {string} path
+ * @param   {callback} middleware - Handle HTTP response
+ */
+router.post("/forget_password", async (req, res) => {
+  try {
+    const result = await sendOTP(req.body);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(err.httpCode || 500).json({ error: err.message });
+  }
+});
+
+/**
+ * Route to reset password
+ * @name api/auth/reset_password
+ * @method POST
+ * @access Public
+ * @inner
+ * @param {string} path
+ * @param   {callback} middleware - Handle HTTP response
+ */
+router.post("/reset_password", async (req, res) => {
+  try {
+    const result = await resetPassword(req.body);
+    res.status(200).json(result);
+  } catch (err) {
+    res
+      .status(err.httpCode || 500)
+      .json({ error: { field: err.field, msg: err.message } });
+  }
+});
+
+/**
  * Route to authenticate credentials
  * @name api/auth
  * @method POST
  * @access Public
  * @inner
  * @param {string} path
- * @param {callback} middleware - Form Parser
  * @param   {callback} middleware - Handle HTTP response
  */
 router.post("/", async (req, res) => {
