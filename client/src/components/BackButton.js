51ccf44f00a719fe0908d528a3b8ee045295f0e7
Sandesh <chapagainsandesh88@gmail.com>
2021-03-14 21:13:02 -0400

    prettify

--- client/src/components/BackButton.js
+++ client/src/components/BackButton.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import { IoArrowBackSharp } from 'react-icons/io5';
-import classNames from 'classnames';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { IoArrowBackSharp } from "react-icons/io5";
+import classNames from "classnames";
+import { useHistory } from "react-router-dom";
 
-export const BackButton = ({className}) => {
-    const history = useHistory();
-    const mainClass = classNames(className,
-        'text-5xl text-black cursor-pointer p-2 rounded-full transition transform hover:text-white hover:bg-kabul'
-        )
-    return (
-        <IoArrowBackSharp onClick={()=>history.goBack()} className={mainClass}/>
-    )
-}
+export const BackButton = ({ className }) => {
+  const history = useHistory();
+  const mainClass = classNames(
+    className,
+    "text-5xl text-black cursor-pointer p-2 rounded-full transition transform hover:text-white hover:bg-kabul"
+  );
+  return (
+    <IoArrowBackSharp onClick={() => history.goBack()} className={mainClass} />
+  );
+};
 export default BackButton;
