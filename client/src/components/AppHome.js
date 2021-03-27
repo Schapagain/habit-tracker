fe961c09266f384f4ff628707f02d548d545ebfd
Sandesh <chapagainsandesh88@gmail.com>
2021-03-26 20:59:57 -0400

    prettify

--- client/src/components/AppHome.js
+++ client/src/components/AppHome.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React from "react";
 // import GoogleLogin from './GoogleLogin';
-import {Route, Switch} from 'react-router-dom';
-import Landing from './Landing';
-import HabitHome from './HabitHome';
-import About from './About';
-import Features from './Features';
-import Login from './Login';
-import Signup from './Signup';
-import Home from './Home';
-import AddHabitForm from './AddHabitForm';
-import ProtectedRoute from './ProtectedRoute';
+import { Route, Switch } from "react-router-dom";
+import Landing from "./Landing";
+import HabitHome from "./HabitHome";
+import About from "./About";
+import Features from "./Features";
+import Login from "./Login";
+import Signup from "./Signup";
+import Home from "./Home";
+import AddHabitForm from "./AddHabitForm";
+import ProtectedRoute from "./ProtectedRoute";
 
 const AppHome = (props) => {
-  const mainClass = "flex mx-auto max-w-screen-xl justify-between flex-col min-h-screen bg-spring-rain w-full"
+  const mainClass =
+    "flex mx-auto max-w-screen-xl justify-between overflow-hidden flex-col min-h-screen bg-spring-rain w-full";
   return (
     <div className={mainClass}>
       <Switch>
@@ -22,12 +23,11 @@ const AppHome = (props) => {
         <Route path="/features" exact component={Features} />
         <Route path="/login" exact component={Login} />
         <Route path="/signup" exact component={Signup} />
-        <ProtectedRoute path="/home" exact component={Home} /> 
+        <ProtectedRoute path="/home" exact component={Home} />
         <ProtectedRoute path="/addhabit" exact component={AddHabitForm} />
       </Switch>
-      </div>
-    // <GoogleLogin/>
+    </div>
   );
-}
+};
 
 export default AppHome;
