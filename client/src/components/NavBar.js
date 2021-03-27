58a309d19f9ed41ba7c130ab86c759a8ded8d1af
Sandesh <chapagainsandesh88@gmail.com>
2021-03-26 21:00:57 -0400

    prettify

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,77 +1,81 @@
-import { GlobalContext } from '../context/GlobalState';
-import { useContext } from 'react';
-import { useHistory } from 'react-router-dom';
+import { GlobalContext } from "../context/GlobalState";
+import { useContext } from "react";
+import { useHistory } from "react-router-dom";
 
 const Brand = () => {
-    const history = useHistory();
-    return (
-        <a 
-        className = "text-calypso my-auto font-medium text-3xl"
-        href="!#" 
-        onClick={(e)=>{e.preventDefault();history.push("/")}}
-        >
-            BlockByBlock
-        </a>
-    );
-}
+  const history = useHistory();
+  return (
+    <a
+      className="text-calypso my-auto font-medium text-3xl"
+      href="!#"
+      onClick={(e) => {
+        e.preventDefault();
+        history.push("/");
+      }}
+    >
+      BlockByBlock
+    </a>
+  );
+};
 
-const NavLink = ({text,onClick}) => {
-    return (
-        <a 
-        className = {
-            `p-2 font-medium text-calypso text-lg mx-4
-            transition ease-in-out duration-700 hover:bg-calypso hover:text-white rounded-xl`
-        }
-        href="!#" 
-        onClick={(e)=>{e.preventDefault();onClick()}}
-        >
-            {text}
-        </a>
-    )
-}
+const NavLink = ({ text, onClick }) => {
+  return (
+    <a
+      className={`p-2 font-medium text-calypso text-lg mx-4
+            transition ease-in-out duration-700 hover:bg-calypso hover:text-white rounded-xl`}
+      href="!#"
+      onClick={(e) => {
+        e.preventDefault();
+        onClick();
+      }}
+    >
+      {text}
+    </a>
+  );
+};
 
 const NavLinks = () => {
-    const { isAuthenticated,logoutUser } = useContext(GlobalContext);
+  const { isAuthenticated, logoutUser } = useContext(GlobalContext);
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const goToFeatures = () => {
-        history.push("/features");
-    }
+  const goToFeatures = () => {
+    history.push("/features");
+  };
 
-    const goToAbout = () => {
-        history.push("/about");
-    }
+  const goToAbout = () => {
+    history.push("/about");
+  };
 
-    const handleLogout = () => {
-        logoutUser();
-        history.push("/");
-    }
+  const handleLogout = () => {
+    logoutUser();
+    history.push("/");
+  };
 
-    const goToLogin = () => {
-        history.push("/login");
-    }
+  const goToLogin = () => {
+    history.push("/login");
+  };
 
-    return (
-        <div className="flex">
-            <NavLink text="Features" onClick={goToFeatures} />
-            <NavLink text="About" onClick={goToAbout} />
-            {isAuthenticated 
-            ? <NavLink text="Logout" onClick={handleLogout} />
-            : <NavLink text="Login" onClick={goToLogin} />
-            }
-        </div>
-    );
-}
+  return (
+    <div className="hidden md:flex">
+      <NavLink text="Features" onClick={goToFeatures} />
+      <NavLink text="About" onClick={goToAbout} />
+      {isAuthenticated ? (
+        <NavLink text="Logout" onClick={handleLogout} />
+      ) : (
+        <NavLink text="Login" onClick={goToLogin} />
+      )}
+    </div>
+  );
+};
 
 const NavBar = () => {
-
-    return (
-        <div className = "w-full z-50 bg-spring-rain max-w-screen-xl fixed top-0 flex p-3 justify-between">
-            <Brand />
-            <NavLinks />
-        </div>
-    );
-}
+  return (
+    <div className="w-full z-50 bg-spring-rain max-w-screen-xl fixed top-0 flex p-3 justify-between">
+      <Brand />
+      <NavLinks />
+    </div>
+  );
+};
 
 export default NavBar;
